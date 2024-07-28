013c67c488f54e4389597725a17156907fe37d9a
swastikkumarpati <swastikkp02@gmail.com>
2024-07-28 06:08:24 +0530

    Img add

--- frontend/src/components/Register/Register.jsx
+++ frontend/src/components/Register/Register.jsx
@@ -280,8 +280,8 @@ export function Register() {
         </div>
         <div className="h-full w-full">
           <img
-            className="mx-auto h-full w-full rounded-md object-cover"
-            src="https://images.unsplash.com/photo-1559526324-4b87b5e36e44?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80"
+            className="mx-auto h-full w-full rounded-md object-cover "
+            src="https://rohinifoundation.org/images/gallery/jul1.jpeg"
             alt=""
           />
         </div>
